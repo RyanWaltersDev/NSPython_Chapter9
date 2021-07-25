88c4409a4722d3b0cb44cc04bc1e8ca647c5ac1d
Ryan Walters <RyanWaltersDev@gmail.com>
2021-07-24 20:33:16 -0400

    +Add IceCreamStand class as per TIY 9-6

--- restaurant.py
+++ restaurant.py
@@ -28,6 +28,19 @@ class Restaurant:
     def increment_number_served(self, customers_served):
         self.number_served += customers_served
 
+class IceCreamStand(Restaurant):
+    '''Model a small ice cream stand'''
+
+    def __init__(self, restaurant_name, cuisine_type, flavors=None, 
+                number_served=0):
+        super().__init__(restaurant_name, cuisine_type, number_served)
+        self.flavors = flavors
+
+    def display_flavors(self, flavors):
+        '''Print a current list of flavors'''
+        print("Below is a list of our available flavors:")
+        print(", " .join(flavors))
+
 # Define instances
 med_restaurant = Restaurant('Metropolis Cafe', 'Mediterranean')
 mex_restaurant = Restaurant('Bollywood Tacos', 'Mexican/Indian Fusion')
@@ -62,5 +75,11 @@ new_restaurant.increment_number_served(67)
 new_restaurant.open_restaurant()
 new_restaurant.increment_number_served(450)
 new_restaurant.open_restaurant()
+
+# Ice cream stand instance and method calls.
+ice_cream_stand = IceCreamStand("Bubba's Ice Cream", "Ice Cream Stand", )
+ice_cream_stand.describe_restaurant()
+ice_cream_stand.open_restaurant()
+ice_cream_stand.display_flavors(['Chocolate', 'Vanilla', 'Strawberry'])
 
 # END OF PROGRAM
